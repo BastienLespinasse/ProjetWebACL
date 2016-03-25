20c5a2b2239d952aa056027c813c5ce9c79e23aa
maxczd <maxime.cazade@orange.fr>
2016-03-25 10:03:21 +0100

    Added files via upload
    Ajout du projet web, 25/03/2016

---
+++ Projet/JS/page1.js
@@ -0,0 +1,24 @@
+function couleur_bouton1()
+{
+	document.getElementById("bouton1").className = "fond2";
+}
+
+function retour_couleur1()
+{
+	document.getElementById("bouton1").className = "fond1";
+}
+
+function couleur_bouton2()
+{
+	document.getElementById("bouton2").className = "fond2";
+}
+
+function retour_couleur2()
+{
+	document.getElementById("bouton2").className = "fond1";
+}
+
+function scroll()
+{
+	window.scrollTo(0, 650);
+}
