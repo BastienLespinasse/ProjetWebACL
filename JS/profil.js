268fc46d5cbe300c6aa601443f30f17a36da0830
BastienLespinasse <blespinasse@hotmail.fr>
2016-04-14 14:28:43 +0200

    Update profil.js

--- JS/profil.js
+++ JS/profil.js
@@ -1,42 +1,6 @@
-function jaime_rouge()
-{
-	if (document.getElementById("jaime_ico").value == 0 ) 
-	{
-		document.getElementById("jaime_ico").src = "../images/jaime_rouge.png";
-		document.getElementById("jaime_ico").value = 1;
-	}
-	else
-	{
-		document.getElementById("jaime_ico").src = "../images/jaime_blanc.png";
-		document.getElementById("jaime_ico").value = 0;
-	}
-}
 
-function cadenas_jaune()
-{
-	if (document.getElementById("cadenas_ico").value == 0 ) 
-	{
-		document.getElementById("cadenas_ico").src = "../images/cadenasJ.png";
-		document.getElementById("cadenas_ico").value = 1;
-	}
 
-	else 
-	 {
-	 	document.getElementById("cadenas_ico").src = "../images/cadenas.png";
-	 	document.getElementById("cadenas_ico").value = 0;
-	 }
-	
-}
 
-function barre()
-{
-	document.getElementById("barre").style.visibility = "visible";
-}
-
-function non_barre(barre)
-{
-	document.getElementById("barre").style.visibility = "hidden";
-}
 
 function deconnexion()
 {
@@ -50,3 +14,87 @@ function deconnexion()
    		deconnexion = false;
   	}
 }
+
+/********* AFFICHAGE DES CHAMPS DE MODIFICATIONS DANS PARAMETRES *********/
+
+$(".img").click(function(){
+  $(this).closest("td").next().children().css("visibility", "visible");
+});
+
+$(".save").click(function(){
+  $(this).css("visibility", "hidden");
+  $(this).closest("td").children().css("visibility", "hidden");
+});
+
+/********* BARRE DES PHOTOS *********/
+
+$(".barre").mouseover(function(){
+  $(this).children("div").css("visibility", "visible");
+});
+
+$(".barre").mouseout(function(){
+  $(this).children("div").css("visibility", "hidden");
+});
+
+
+/********* COEUR ROUGE/BLANC *********/
+
+var saveSrc_coeur = $(".coeur").attr('src'); 
+
+$(".coeur").click(function() { 
+
+var obj_coeur = $(this);
+
+if (saveSrc_coeur != obj_coeur.attr('src')) 
+	{
+	 $(this).attr('src','../images/jaime_blanc.png');
+	} 
+else 
+{
+	 $(this).attr('src','../images/jaime_rouge.png');
+}
+
+});
+
+/********* CADENAS JAUNE/BLANC *********/
+
+var saveSrc_cadenas = $(".cadenas").attr('src'); 
+
+$(".cadenas").click(function() { 
+
+var obj_cadenas = $(this);
+
+if (saveSrc_cadenas != obj_cadenas.attr('src')) 
+	{
+	 $(this).attr('src','../images/cadenas.png');
+	} 
+else 
+{
+	 $(this).attr('src','../images/cadenasJ.png');
+}
+
+});
+
+/********* PUNAISE JAUNE/BLANCHE *********/
+
+var saveSrc_punaise = $(".punaise").attr('src'); 
+
+$(".punaise").click(function() { 
+
+var obj_punaise = $(this);
+
+if (saveSrc_punaise != obj_punaise.attr('src')) 
+	{
+	 $(this).attr('src','../images/punaise.png');
+	} 
+else 
+{
+	 $(this).attr('src','../images/punaiseJ.png');
+}
+
+});
+
+
+
+
+
